c4e1b2c476b6cb1b19530bccb51e038e03254435
Dimitris Tsalapatas <dhmhtrhs393@gmail.com>
2023-04-30 15:28:42 +0300

    initial for linux

--- src/hw3_package/hw3_main.java
+++ src/hw3_package/hw3_main.java
@@ -4,6 +4,23 @@ public class hw3_main
 {
 	public static void main (String []args)
 	{
-		
+		System.out.println("--- AI ---");
+		System.out.println("------------------------------");
+		System.out.println("|   |   |   |   |   |   |   |");
+		System.out.println("------------------------------");
+		System.out.println("|   |   |   |   |   |   |   |");
+		System.out.println("------------------------------");
+		System.out.println("|   |   |   |   |   |   |   |");
+		System.out.println("------------------------------");
+		System.out.println("|   |   |   |   |   |   |   |");
+		System.out.println("------------------------------");
+		System.out.println("|   |   |   |   |   |   |   |");
+		System.out.println("------------------------------");
+		System.out.println("|   |   |   |   |   |   |   |");
+		System.out.println("------------------------------");
+		System.out.println("|   |   |   |   |   |   |   |");
+		System.out.println("------------------------------");
+		System.out.println("|   |   |   |   |   |   |   |");
+		System.out.println("------------------------------");
 	}
 }
